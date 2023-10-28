57f29ef717bea893c604173219f826d90ef1a62c
Kuzinatra22 <144513446+Kuzinatra22@users.noreply.github.com>
2023-10-28 16:48:49 +1000

    Add files via upload

---
+++ soushin22-main/pokm.py
@@ -0,0 +1,27 @@
+# bukva = input("Введите букву")
+# if bukva == "a" or bukva == "e" or bukva == "i" or bukva == "o":
+#     print("Буква гласная")
+# else:
+#     print("Буква согласная")
+
+
+# bukva = input("Введите букву")
+
+# glasnye = ["a","e","i","o","y"]
+
+# if bukva in glasnye:
+#     print("Буква гласная")
+# else:
+#     print("Буква согласная")
+
+
+coordinate = input("Введите координату ")
+bukva = coordinate[0]
+number = int(coordinate[1]) 
+
+if 100 % 2 == 0:
+    print ("клетка белая")
+else:
+    print("клетка чёрная")
+
+
