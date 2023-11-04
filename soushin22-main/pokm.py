f04c9a4b4385403295e63c2489e102691b4185d0
Kuzinatra22 <144513446+Kuzinatra22@users.noreply.github.com>
2023-11-04 17:36:37 +1100

    Add files via upload

--- soushin22-main/pokm.py
+++ soushin22-main/pokm.py
@@ -16,12 +16,27 @@
 
 
 coordinate = input("Введите координату ")
-bukva = coordinate[0]
+letter = coordinate[0]
 number = int(coordinate[1]) 
 
-if 100 % 2 == 0:
-    print ("клетка белая")
+if number % 2 == 0 and (letter == "b" or letter == "d" or letter == "f" or letter == "h") :
+    print ("клетка чёрная")
+elif: number % 2 == 0 and (letter == "a" or letter == "c" or letter == "e" or letter == "g") :
 else:
     print("клетка чёрная")
 
+
+
+num = 121
+count = 0
+for i in range(2,num):
+    if (num % i == 0):
+        count += 1
+    if(count>0):
+        print(f"Число {num} составное.\nОно делится на {i}")
+    else:
+         print(f"число простое")
+
+  
+       
 
