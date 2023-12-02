cc8fd4d13cc10b8b9255c86a2c7324a2bf4b52d0
Kuzinatra22 <144513446+Kuzinatra22@users.noreply.github.com>
2023-12-02 16:38:54 +1000

    Add files via upload

---
+++ soushin22-main/soushin22-main/figura.py
@@ -0,0 +1,4 @@
+storona=int(input("Введите число "))
+if storona == 3:
+    print ("треугольник")
+
---
+++ soushin22-main/soushin22-main/middle_num.py
@@ -0,0 +1,23 @@
+ a = int(input("Введите число"))
+ b = int(input("Введите число"))
+ c = int(input("Введите число"))
+ d = int(input("Введите число"))
+
+ mid_num = (a + b + c + d)/4
+ print (round(mid_num,2))
+col = 0
+
+a = int(input("Введите число")
+col = col + 1
+sum = 0
+
+while a != 0:
+print("Вы ввели", col, "чисел")
+sum = sum + a
+    print("Сумма чисел равна" , sum)
+    print("Цикл продолжается")
+    a = int(input("Введите число а"))
+    col = sum
+
+midl = sum/col
+print(midl)
