45dbbb51cc0973e92f1292f3f901b310a318943c
Kuzinatra22 <144513446+Kuzinatra22@users.noreply.github.com>
2023-12-09 17:34:10 +1100

    Add files via upload

---
+++ soushin22-main/soushin22-main/graphic.py
@@ -0,0 +1,46 @@
+import turtle
+from turtle import *
+import time
+from time import sleep
+# forward(90)
+# left(90)
+# forward(90)
+# right(90)
+# back(90)
+# right(90)
+# forward(90)
+
+чрпх = Turtle()
+
+def Квадрат():
+    for i in range
+
+for i in range(0,4):
+    чрпх.forward(100)
+    чрпх.left(90)
+penup()
+goto(0,-100)
+pendown()
+for i in range(4):
+    forward(100)
+    left(90)
+penup()
+goto(-200)
+
+
+
+
+# print(чрпх.pos())
+# чрпх.setpos(200,50)
+
+
+
+
+
+
+
+
+
+
+
+turtle.extionoclick()
---
+++ soushin22-main/soushin22-main/soushin22-main/palendrom.py
@@ -0,0 +1,9 @@
+word = "наворован"
+
+left = 0
+right = 8
+
+while left != right:
+    if (word[left]==word{right}):
+        print(f"буквы")
+
