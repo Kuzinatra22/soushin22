cbaa3baa2a17637aab1b799e159479729d1939d0
Kuzinatra22 <144513446+Kuzinatra22@users.noreply.github.com>
2023-12-16 16:39:59 +1000

    Delete soushin22-main directory

--- soushin22-main/soushin22-main/figura.py
+++
@@ -1,4 +0,0 @@
-storona=int(input("Введите число "))
-if storona == 3:
-    print ("треугольник")
-
--- soushin22-main/soushin22-main/palendrom.py
+++
@@ -1,9 +0,0 @@
-word = "наворован"
-
-left = 0
-right = 8
-
-while left != right:
-    if (word[left]==word{right}):
-        print(f"буквы")
-
